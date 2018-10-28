98fca97b619e3432f6afdc52cf369060134d5439
alphaflyer <37005273+alphaflyer@users.noreply.github.com>
2018-10-28 12:06:26 +0100

    new version
    got rid of global variables and made some smaller modifications. should run now more efficient.

--- sun_gen.py
+++ sun_gen.py
@@ -1,20 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 import random, MySQLdb, array, math, uuid, string
 
-global comm
 comm =[[],[]]
 generator = 0
 
-def db_con():
-    
-    global db
-    global cur
+db = MySQLdb.connect('localhost','universe','F7V4Z50Jl7HyKFI8','universe')
+cur = db.cursor()
 
-    db = MySQLdb.connect('localhost','res','1234','universe')
-    cur = db.cursor()
-
-def collect_suns():
+def collect_suns(cur):
 
     sql_query = ("SELECT SUN_CLASS, Commonness FROM sun_classes")
     cur.execute(sql_query)
@@ -24,22 +18,19 @@ def collect_suns():
         comm[0].append(row[0])
         comm[1].append(int(row[1]*100000))
 
-def find_sun():
+    return comm
 
-    global sun
-     
+def find_sun(comm):
+
     sunclass = sum(([t] * w for t, w in zip(*comm)), [])
     sun = random.choice(sunclass)
     
     print("Success! Found new sun")
-    print("Sun Class is: ", sun)
+    print("Sun Class is: %s") % sun
 
-def create_sun():
+    return sun
 
-    global temp
-    global mass
-    global radius
-    global lumi
+def create_sun(cur, sun):
 
     sql_query = ("SELECT SUN_CLASS, Temp_min, Temp_max, Mass_min, Mass_max, Radius_min, Radius_max, Luminosity_min, Lumiosity_max FROM sun_classes WHERE SUN_CLASS='%s'" % sun)
 
@@ -47,7 +38,8 @@ def create_sun():
     suns = cur.fetchall()
 
     for row in suns:
-        print("Importing Meta data: ", row)
+        print("Importing Meta data...") 
+        print row
 
     print("Generating attributes for Sun...")
 
@@ -58,24 +50,30 @@ def create_sun():
 
     print("Sun temperature is %s K, Mass is %s, Radius is %s, Luminosity is %s" % (temp, mass, radius, lumi))
 
-def sun_name():
+    return temp, mass, radius, lumi
 
-    global letters
-    global digits
-    global name
-    global sunid
+def sun_name():
 
     letters = ''.join(random.choice('FXGJAKSDF') for i in range(5))
     digits = random.randint(10000,99999)
     name = "4FX-%s-%d" % (letters,digits)
-    sunid = str(uuid.uuid4())
-    
-    print("Sun's name is now %s and its UUID is %s" % (name, sunid))   
+        
+    print("Sun's name is now %s") % name
 
-def write_sun():
+    return name
+
+def sun_id():
+    
+    sunid = str(uuid.uuid4())
+
+    print("Sun's ID is now %s") % sunid
+
+    return sunid
+
+def write_sun(sunid, sun, name, temp, mass, radius, lumi, db):
 
     sql_query = "INSERT INTO suns (SUN_ID,SUN_CLASS,Sun_Name,Sun_Temp,Sun_Mass,Sun_Radius,Sun_Luminosity) VALUES (%s,%s,%s,%s,%s,%s,%s)" 
-    val = sunid,sun,name,temp,mass,radius,lumi
+    val = sunid, sun, name, temp, mass, radius, lumi
     
     cur.execute(sql_query, val)
     
@@ -83,15 +81,17 @@ def write_sun():
 
     print("New Sun created and stored in Database!")
 
+    return db
 
-db_con()
-collect_suns()
 
-while generator < 10000:
-    find_sun()
-    create_sun()
-    sun_name()
-    write_sun()
+collect_suns(cur)
+
+while generator < 2:
+    sun = find_sun(comm)
+    temp, mass, radius, lumi = create_sun(cur, sun)
+    name = sun_name()
+    sunid = sun_id()
+    write_sun(sunid, sun, name, temp, mass, radius, lumi, db)
     
     generator += 1
     print("Currently %d suns created!" % generator)
