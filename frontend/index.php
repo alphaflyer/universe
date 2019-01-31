d8b6d9be118a383632dd609c14c9a816b79cc439
EC2 Default User <ec2-user@ip-172-31-41-112.eu-central-1.compute.internal>
2019-01-31 22:20:45 +0000

    modified

--- frontend/index.php
+++ frontend/index.php
@@ -17,7 +17,7 @@
 <div class="flex-grid">
   <div class="col"><a href="system_vis_hq_v2.php">SYSTEM VIS<br>HQ v25<br><p>max. Star count = 10000<br><br>particle system</p></a></div>
   <div class="col"><a href="star_vis.php">STAR VIS</a></div>
-  <div class="col"><a href="../phpMyAdmin">DATABASE</a></div>
+  <div class="col"><a href="../../../phpMyAdmin">DATABASE</a></div>
 </div>
 
 
